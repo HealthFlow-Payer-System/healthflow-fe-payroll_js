97cecadf1eeb4924f8cc5607d4ae32c4c1a7a22e
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-07-18 18:04:59 +0530

    migrated form the cra to vite

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import pkg from './package.json';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.jsx'),
+      name: 'Payroll',
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: [
+        /^@babel.*/,
+        /^@date-io\/.*/,
+        /^@mui\/material.*/,
+        /^@mui\/icons-material.*/,
+        /^@material-ui\/.*/,
+        /^@openimis.*/,
+        'classnames',
+        'clsx',
+        'history',
+        /^lodash.*/,
+        'moment',
+        'prop-types',
+        /^react.*/,
+        /^redux.*/,
+      ],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+    sourcemap: true,
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
